9c7ffc0947bbd0c280895a25155fae9187239af3
Edward A Maxedon <edadma@gmail.com>
2024-12-29 22:06:36 -0500

    implement router

--- apion/src/main/scala/io/github/edadma/apion/Router.scala
+++ apion/src/main/scala/io/github/edadma/apion/Router.scala
@@ -10,11 +10,20 @@ private case class StaticSegment(value: String) extends RouteSegment
 private case class ParamSegment(name: String)   extends RouteSegment
 private case class WildcardSegment()            extends RouteSegment
 
-private case class Route(
+trait Process {
+  val handler: Handler
+}
+
+private case class Endpoint(
     method: String,
     segments: List[RouteSegment],
     handler: Handler,
-)
+) extends Process
+
+private case class Route(
+    segments: List[RouteSegment],
+    handler: Handler,
+) extends Process
 
 object Router:
   private def parsePath(path: String): List[RouteSegment] =
@@ -27,36 +36,56 @@ object Router:
       }.toList
 
 private class Router extends Handler:
-  private val routes = scala.collection.mutable.ListBuffer[Route]()
+  private val routesBuffer = scala.collection.mutable.ListBuffer[Process]()
+  private lazy val routes  = routesBuffer.toList
 
-  def apply(r: Request): Future[Result] = Future(Skip)
+  def apply(request: Request): Future[Result] =
+    def processNext(ps: List[Process], req: Request): Future[Result] =
+      ps match
+        case Nil => Future.successful(Skip)
+        case p :: rest =>
+          val result: Future[Result] =
+            p match
+              case Endpoint(method, segments, handler) =>
+                handler(req)
+              case Route(segments, handler) =>
+                handler(req)
+              case _ => p.handler(req)
 
-  private def addRoute(method: String, path: String, handler: Handler): Router =
-    routes += Route(method, Router.parsePath(path), handler)
+          result.map {
+            case Continue(newReq) => processNext(rest, newReq)
+            case Skip             => processNext(rest, req)
+            case result           => Future.successful(result)
+          }
+
+    processNext(routes, request)
+
+  private def addEndpoint(method: String, path: String, handler: Handler): Router =
+    routesBuffer += Endpoint(method, Router.parsePath(path), handler)
     this
 
-  def get(path: String, handler: Handler): Router    = addRoute("GET", path, handler)
-  def post(path: String, handler: Handler): Router   = addRoute("POST", path, handler)
-  def put(path: String, handler: Handler): Router    = addRoute("PUT", path, handler)
-  def delete(path: String, handler: Handler): Router = addRoute("DELETE", path, handler)
-  def patch(path: String, handler: Handler): Router  = addRoute("PATCH", path, handler)
+  def get(path: String, handler: Handler): Router    = addEndpoint("GET", path, handler)
+  def post(path: String, handler: Handler): Router   = addEndpoint("POST", path, handler)
+  def put(path: String, handler: Handler): Router    = addEndpoint("PUT", path, handler)
+  def delete(path: String, handler: Handler): Router = addEndpoint("DELETE", path, handler)
+  def patch(path: String, handler: Handler): Router  = addEndpoint("PATCH", path, handler)
 
-  private[apion] def matchRoute(method: String, path: String): Option[(Handler, Map[String, String], List[String])] =
-    val segments = path.split("/").filter(_.nonEmpty).toList
-
-    @tailrec
-    def matchSegments(
-        routeSegs: List[RouteSegment],
-        pathSegs: List[String],
-        params: Map[String, String],
-    ): Option[(Map[String, String], List[String])] =
-      (routeSegs, pathSegs) match
-        case (Nil, remaining)                                    => Some((params, remaining))
-        case (WildcardSegment() :: rs, _ :: ps)                  => matchSegments(rs, ps, params)
-        case (StaticSegment(exp) :: rs, act :: ps) if exp == act => matchSegments(rs, ps, params)
-        case (ParamSegment(name) :: rs, act :: ps)               => matchSegments(rs, ps, params + (name -> act))
-        case _                                                   => None
-
-    routes.find(r => r.method == method).flatMap { route =>
-      matchSegments(route.segments, segments, Map()).map((params, rem) => (route.handler, params, rem))
-    }
+//  private[apion] def matchRoute(method: String, path: String): Option[(Handler, Map[String, String], List[String])] =
+//    val segments = path.split("/").filter(_.nonEmpty).toList
+//
+//    @tailrec
+//    def matchSegments(
+//        routeSegs: List[RouteSegment],
+//        pathSegs: List[String],
+//        params: Map[String, String],
+//    ): Option[(Map[String, String], List[String])] =
+//      (routeSegs, pathSegs) match
+//        case (Nil, remaining)                                    => Some((params, remaining))
+//        case (WildcardSegment() :: rs, _ :: ps)                  => matchSegments(rs, ps, params)
+//        case (StaticSegment(exp) :: rs, act :: ps) if exp == act => matchSegments(rs, ps, params)
+//        case (ParamSegment(name) :: rs, act :: ps)               => matchSegments(rs, ps, params + (name -> act))
+//        case _                                                   => None
+//
+//    routes.find(r => r.method == method).flatMap { route =>
+//      matchSegments(route.segments, segments, Map()).map((params, rem) => (route.handler, params, rem))
+//    }

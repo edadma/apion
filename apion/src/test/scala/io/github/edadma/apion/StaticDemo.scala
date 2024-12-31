8ce4a886d3eb35727c71ecb25b1873df7c872fa6
Edward A Maxedon <edadma@gmail.com>
2024-12-31 14:12:05 -0500

    fix finalizer issue

--- apion/src/test/scala/io/github/edadma/apion/StaticDemo.scala
+++ apion/src/test/scala/io/github/edadma/apion/StaticDemo.scala
@@ -3,14 +3,7 @@ package io.github.edadma.apion
 object StaticDemo:
   def run(): Unit =
     val server = Server()
-//      .use(StaticMiddleware("project"))
-      .use(SecurityMiddleware())
-      .get(
-        "/",
-        req =>
-          println(req)
-          "Hello, World!".asText,
-      )
+      .use(StaticMiddleware("project"))
 
     server.listen(3000) {
       println("Static file server running at http://localhost:3000")

cc8c3ce190ebf2c2c80f6e304fd61f54c29e6bb7
Edward A Maxedon <edadma@gmail.com>
2025-01-03 08:57:16 -0500

    fix response header handling

--- apion/src/main/scala/io/github/edadma/apion/Response.scala
+++ apion/src/main/scala/io/github/edadma/apion/Response.scala
@@ -8,6 +8,8 @@ case class Response(
     headers: Map[String, List[String]], // = Map("content-type" -> Seq("application/json")),
     body: String,
 ):
+  def header(name: String): Option[String] = headers.get(name.toLowerCase).map(_.head)
+
   def header(name: String, value: String): Response =
     val key = name.toLowerCase
 
@@ -27,11 +29,11 @@ object Response:
 
   // Global configuration for default headers
   private var defaultHeaders = Seq(
-    "Server"        -> "Apion",
-    "Cache-Control" -> "no-store, no-cache, must-revalidate, max-age=0",
-    "Pragma"        -> "no-cache",
-    "Expires"       -> "0",
-    "X-Powered-By"  -> "Apion",
+    "server"        -> "Apion",
+    "cache-control" -> "no-store, no-cache, must-revalidate, max-age=0",
+    "pragma"        -> "no-cache",
+    "expires"       -> "0",
+    "x-powered-by"  -> "Apion",
   )
 
   /** Configure global default headers
@@ -66,11 +68,13 @@ object Response:
   ): Response =
     Response(
       status = status,
-      headers = standardHeaders
-        .add("Content-Type", "application/json")
-        .addAll(additionalHeaders),
+      headers =
+        makeHeaders(standardHeaders.appended("Content-Type" -> "application/json").appendedAll(additionalHeaders)),
       body = data.toJson,
     )
+
+  private def makeHeaders(hs: Seq[(String, String)]): Map[String, List[String]] =
+    hs.map((k, v) => k.toLowerCase -> List(v)).toMap
 
   /** Create a plain text response with standard headers
     * @param content
@@ -87,9 +91,7 @@ object Response:
   ): Response =
     Response(
       status = status,
-      headers = standardHeaders
-        .add("Content-Type", "text/plain")
-        .addAll(additionalHeaders),
+      headers = makeHeaders(standardHeaders.appended("Content-Type" -> "text/plain").appendedAll(additionalHeaders)),
       body = content,
     )
 

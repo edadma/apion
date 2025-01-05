de77f5deb09fd7cf37d98462301645219aca8a0b
Edward A Maxedon <edadma@gmail.com>
2025-01-05 14:30:14 -0500

    add support to 'fs' facade

--- nodejs/src/main/scala/io/github/edadma/nodejs/fs.scala
+++ nodejs/src/main/scala/io/github/edadma/nodejs/fs.scala
@@ -10,11 +10,10 @@ object fs extends js.Object:
 
 @js.native
 trait FSPromises extends js.Object:
-  def readFile(path: String): js.Promise[Buffer] = js.native
-
+  def createReadStream(path: String): ReadableStream                                = js.native
+  def readFile(path: String): js.Promise[Buffer]                                    = js.native
   def readFile(path: String, options: ReadFileOptions): js.Promise[String | Buffer] = js.native
-
-  def stat(path: String): js.Promise[Stats] = js.native
+  def stat(path: String): js.Promise[Stats]                                         = js.native
 
 @js.native
 trait ReadFileOptions extends js.Object:
@@ -33,11 +32,18 @@ object ReadFileOptions:
 @js.native
 trait Stats extends js.Object:
   def isDirectory(): Boolean = js.native
+  def isFile(): Boolean      = js.native
+  def size: Double           = js.native
+  def mode: Double           = js.native
+  def mtime: js.Date         = js.native
 
-  def isFile(): Boolean = js.native
+@js.native
+trait ReadableStream extends js.Object {
+  def pipe(destination: WritableStream): Unit                            = js.native
+  def on(event: String, callback: js.Function1[js.Any, Unit]): this.type = js.native
+}
 
-  def size: Double = js.native
-
-  def mode: Double = js.native
-
-  def mtime: js.Date = js.native
+@js.native
+trait WritableStream extends js.Object {
+  def end(): Unit = js.native
+}
